865371c34359cdd5591c705b3248e6561af3617b
Ray <hjc440@nyu.edu>
2016-08-15 11:28:44 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,655 @@
+//sound effect
+var sounda = new Howl({
+  	urls: ['a.wav']
+	});
+var soundb = new Howl({
+  	urls: ['b.wav']
+	});
+var soundc = new Howl({
+  	urls: ['c.wav']
+	});
+var soundd = new Howl({
+  	urls: ['d.wav']
+	});
+var sounde = new Howl({
+  	urls: ['e.wav']
+	});
+var soundf = new Howl({
+  	urls: ['f.wav']
+	});
+var soundg = new Howl({
+  	urls: ['g.wav']
+	});
+var soundh = new Howl({
+  	urls: ['h.wav']
+	});
+var soundi = new Howl({
+  	urls: ['i.wav']
+	});
+var soundj = new Howl({
+  	urls: ['j.wav']
+	});
+var soundk = new Howl({
+  	urls: ['k.wav']
+	});
+var soundl = new Howl({
+  	urls: ['l.wav']
+	});
+var soundm = new Howl({
+  	urls: ['m.wav']
+	});
+var soundn = new Howl({
+  	urls: ['n.wav']
+	});
+var soundo = new Howl({
+  	urls: ['o.wav']
+	});
+var soundp = new Howl({
+  	urls: ['p.wav']
+	});
+var soundq = new Howl({
+  	urls: ['q.wav']
+	});
+var soundr = new Howl({
+  	urls: ['r.wav']
+	});
+var sounds = new Howl({
+  	urls: ['s.wav']
+	});
+var soundt = new Howl({
+  	urls: ['t.wav']
+	});
+var soundu = new Howl({
+  	urls: ['u.wav']
+	});
+var soundv = new Howl({
+  	urls: ['v.wav']
+	});
+var soundw = new Howl({
+  	urls: ['w.wav']
+	});
+var soundx = new Howl({
+  	urls: ['x.wav']
+	});
+var soundy = new Howl({
+  	urls: ['y.wav']
+	});
+var soundz = new Howl({
+  	urls: ['z.wav']
+	});
+var soundc = new Howl({
+  	urls: ['c.wav']
+	});
+
+// Voice
+var soundaa = new Howl({
+	urls: ['alien.mp3']
+	});
+var soundbb = new Howl({
+	urls: ['bow.mp3']
+	});
+var soundcc = new Howl({
+	urls: ['carousel.mp3']
+	});
+var sounddd = new Howl({
+	urls: ['dove.mp3']
+	});
+var soundee = new Howl({
+	urls: ['elf.mp3']
+	});
+var soundff = new Howl({
+	urls: ['fish.mp3']
+	});
+var soundgg = new Howl({
+	urls: ['garden.mp3']
+	});
+var soundhh = new Howl({
+	urls: ['heart.mp3']
+	});
+var soundii = new Howl({
+	urls: ['island.mp3']
+	});
+var soundjj = new Howl({
+	urls: ['jewelry.mp3']
+	});
+var soundkk = new Howl({
+	urls: ['key.mp3']
+	});
+var soundll = new Howl({
+	urls: ['ladder.mp3']
+	});
+var soundmm = new Howl({
+	urls: ['me.mp3']
+	});
+var soundnn = new Howl({
+	urls: ['nest.mp3']
+	});
+var soundoo = new Howl({
+	urls: ['outer_space.mp3']
+	});
+var soundpp = new Howl({
+	urls: ['parachute.mp3']
+	});
+var soundqq = new Howl({
+	urls: ['queen.mp3']
+	});
+var soundrr = new Howl({
+	urls: ['rain.mp3']
+	});
+var soundss = new Howl({
+	urls: ['star.mp3']
+	});
+var soundtt = new Howl({
+	urls: ['tree.mp3']
+	});
+var sounduu = new Howl({
+	urls: ['unicorn.mp3']
+	});
+var soundvv = new Howl({
+	urls: ['vase.mp3']
+	});
+var soundww = new Howl({
+	urls: ['windmill.mp3']
+	});
+var soundxx = new Howl({
+	urls: ['xylophone.mp3']
+	});
+var soundyy = new Howl({
+	urls: ['yacht.mp3']
+	});
+var soundzz = new Howl({
+	urls: ['z.mp3']
+	});
+
+
+
+// mouse hover to make sound
+$('.a').hover(
+	// mouse over
+	function(){
+	soundaa.play();
+	},
+	//mouse leave
+	function(){
+	soundaa.stop();	
+	}
+	);
+$('.b').hover(
+	function(){
+	soundbb.play();
+	},
+	function(){
+	soundbb.stop();	
+	}
+	);
+$('.c').hover(
+	function(){
+	soundcc.play();
+	},
+	function(){
+	soundcc.stop();	
+	}
+	);
+$('.d').hover(
+	function(){
+	sounddd.play();
+	},
+	function(){
+	sounddd.stop();	
+	}
+	);
+$('.e').hover(
+	function(){
+	soundee.play();
+	},
+	function(){
+	soundee.stop();	
+	}
+	);
+$('.f').hover(
+	function(){
+	soundff.play();
+	},
+	function(){
+	soundff.stop();	
+	}
+	);
+$('.g').hover(
+	function(){
+	soundgg.play();
+	},
+	function(){
+	soundgg.stop();	
+	}
+	);
+$('.h').hover(
+	function(){
+	soundhh.play();
+	},
+	function(){
+	soundhh.stop();	
+	}
+	);
+$('.i').hover(
+	function(){
+	soundii.play();
+	},
+	function(){
+	soundii.stop();	
+	}
+	);
+$('.j').hover(
+	function(){
+	soundjj.play();
+	},
+	function(){
+	soundjj.stop();	
+	}
+	);
+$('.k').hover(
+	function(){
+	soundkk.play();
+	},
+	function(){
+	soundkk.stop();	
+	}
+	);
+$('.l').hover(
+	function(){
+	soundll.play();
+	},
+	function(){
+	soundll.stop();	
+	}
+	);
+$('.m').hover(
+	function(){
+	soundmm.play();
+	},
+	function(){
+	soundmm.stop();	
+	}
+	);
+$('.n').hover(
+	function(){
+	soundnn.play();
+	},
+	function(){
+	soundnn.stop();	
+	}
+	);
+$('.o').hover(
+	function(){
+	soundoo.play();
+	},
+	function(){
+	soundoo.stop();	
+	}
+	);
+$('.p').hover(
+	function(){
+	soundpp.play();
+	},
+	function(){
+	soundpp.stop();	
+	}
+	);
+$('.q').hover(
+	function(){
+	soundqq.play();
+	},
+	function(){
+	soundqq.stop();	
+	}
+	);
+$('.r').hover(
+	function(){
+	soundrr.play();
+	},
+	function(){
+	soundrr.stop();	
+	}
+	);
+$('.s').hover(
+	function(){
+	soundss.play();
+	},
+	function(){
+	soundss.stop();	
+	}
+	);
+$('.t').hover(
+	function(){
+	soundtt.play();
+	},
+	function(){
+	soundtt.stop();	
+	}
+	);
+$('.u').hover(
+	function(){
+	sounduu.play();
+	},
+	function(){
+	sounduu.stop();	
+	}
+	);
+$('.v').hover(
+	function(){
+	soundvv.play();
+	},
+	function(){
+	soundvv.stop();	
+	}
+	);
+$('.w').hover(
+	function(){
+	soundww.play();
+	},
+	function(){
+	soundww.stop();	
+	}
+	);
+$('.x').hover(
+	function(){
+	soundxx.play();
+	},
+	function(){
+	soundxx.stop();	
+	}
+	);
+$('.y').hover(
+	function(){
+	soundyy.play();
+	},
+	function(){
+	soundyy.stop();	
+	}
+	);
+$('.z').hover(
+	function(){
+	soundzz.play();
+	},
+	function(){
+	soundzz.stop();	
+	}
+	);
+
+$(document).ready(function() {
+  $('#imga').hide();
+  $('#imgb').hide();
+  $('#imgc').hide();
+  $('#imgd').hide();
+  $('#imge').hide();
+  $('#imgf').hide();
+  $('#imgg').hide();
+  $('#imgh').hide();
+  $('#imgi').hide();
+  $('#imgj').hide();
+  $('#imgk').hide();
+  $('#imgl').hide();
+  $('#imgm').hide();
+  $('#imgn').hide();
+  $('#imgo').hide();
+  $('#imgp').hide();
+  $('#imgq').hide();
+  $('#imgr').hide();
+  $('#imgs').hide();
+  $('#imgt').hide();
+  $('#imgu').hide();
+  $('#imgv').hide();
+  $('#imgw').hide();
+  $('#imgx').hide();
+  $('#imgy').hide();
+  $('#imgz').hide();
+  	
+  	$(document)
+
+  		.keydown(function(){
+
+  		if(event.keyCode == 65) {
+    	sounda.play();
+		// $(".a").css('color', 'orange');
+		$('#imga').show();
+		}
+    	if(event.keyCode == 66) {
+    	soundb.play();
+		$('#imgb').show();
+		}
+		if(event.keyCode == 67) {
+    	soundc.play();
+		$('#imgc').show();
+		}
+		if(event.keyCode == 68) {
+    	soundd.play();
+		$('#imgd').show();
+		}
+		if(event.keyCode == 69) {
+    	sounde.play();
+		$('#imge').show();
+		}
+		if(event.keyCode == 70) {
+    	soundf.play();
+		$('#imgf').show();
+		}
+		if(event.keyCode == 71) {
+    	soundg.play();
+		$('#imgg').show();
+		}
+		if(event.keyCode == 72) {
+    	soundh.play();
+		$('#imgh').show();
+		}
+		if(event.keyCode == 73) {
+    	soundi.play();
+		$('#imgi').show();
+		}
+		if(event.keyCode == 74) {
+    	soundj.play();
+		$('#imgj').show();
+		}
+		if(event.keyCode == 75) {
+    	soundk.play();
+		$('#imgk').show();
+		}
+		if(event.keyCode == 76) {
+    	soundl.play();
+		$('#imgl').show();
+		}
+		if(event.keyCode == 77) {
+    	soundm.play();
+		$('#imgm').show();
+		}
+		if(event.keyCode == 78) {
+    	soundn.play();
+		$('#imgn').show();
+		}
+		if(event.keyCode == 79) {
+    	soundo.play();
+		$('#imgo').show();
+		}
+		if(event.keyCode == 80) {
+    	soundp.play();
+		$('#imgp').show();
+		}
+		if(event.keyCode == 81) {
+    	soundq.play();
+		$('#imgq').show();
+		}
+		if(event.keyCode == 82) {
+    	soundr.play();
+		$('#imgr').show();
+		}
+		if(event.keyCode == 83) {
+    	sounds.play();
+		$('#imgs').show();
+		}
+		if(event.keyCode == 84) {
+    	soundt.play();
+		$('#imgt').show();
+		}
+		if(event.keyCode == 85) {
+    	soundu.play();
+		$('#imgu').show();
+		}
+		if(event.keyCode == 86) {
+    	soundv.play();
+		$('#imgv').show();
+		}
+		if(event.keyCode == 87) {
+    	soundw.play();
+		$('#imgw').show();
+		}
+		if(event.keyCode == 88) {
+    	soundx.play();
+		$('#imgx').show();
+		}
+		if(event.keyCode == 89) {
+    	soundy.play();
+		$('#imgy').show();
+		}
+		if(event.keyCode == 90) {
+    	soundz.play();
+		$('#imgz').show();
+		}
+		})
+     
+  		.keyup(function(){ 
+
+		if(event.keyCode == 65) {
+	 	//jQuery delay function
+	 	setTimeout(function (){
+	 	// $(".a").css('color', 'black'); 
+	 	$('#imga').hide();
+		}, 3500);
+	 	// sounda.stop();
+		}
+		if(event.keyCode == 66) {
+	 	setTimeout(function (){
+	 	$('#imgb').hide();
+		}, 4500);
+	 	}
+	 	if(event.keyCode == 67) {
+	 	setTimeout(function (){
+	 	$('#imgc').hide();
+		}, 4500);
+	 	}
+	 	if(event.keyCode == 68) {
+	 	setTimeout(function (){
+	 	$('#imgd').hide();
+		}, 4500);
+	 	}
+		if(event.keyCode == 69) {
+	 	setTimeout(function (){
+	 	$('#imge').hide();
+		}, 3000);
+	 	}
+	 	if(event.keyCode == 70) {
+	 	setTimeout(function (){
+	 	$('#imgf').hide();
+		}, 4000);
+	 	}
+	 	if(event.keyCode == 71) {
+	 	setTimeout(function (){
+	 	$('#imgg').hide();
+		}, 3500);
+	 	}
+	 	if(event.keyCode == 72) {
+	 	setTimeout(function (){
+	 	$('#imgh').hide();
+		}, 5000);
+	 	}
+	 	if(event.keyCode == 73) {
+	 	setTimeout(function (){
+	 	$('#imgi').hide();
+		}, 5000);
+	 	}
+	 	if(event.keyCode == 74) {
+	 	setTimeout(function (){
+	 	$('#imgj').hide();
+		}, 4000);
+	 	}
+	 	if(event.keyCode == 75) {
+	 	setTimeout(function (){
+	 	$('#imgk').hide();
+		}, 3000);
+	 	}
+	 	if(event.keyCode == 76) {
+	 	setTimeout(function (){
+	 	$('#imgl').hide();
+		}, 2000);
+	 	}
+	 	if(event.keyCode == 77) {
+	 	setTimeout(function (){
+	 	$('#imgm').hide();
+		}, 3500);
+	 	}
+	 	if(event.keyCode == 78) {
+	 	setTimeout(function (){
+	 	$('#imgn').hide();
+		}, 4000);
+	 	}
+	 	if(event.keyCode == 79) {
+	 	setTimeout(function (){
+	 	$('#imgo').hide();
+		}, 4000);
+	 	}
+	 	if(event.keyCode == 80) {
+	 	setTimeout(function (){
+	 	$('#imgp').hide();
+		}, 6000);
+	 	}
+	 	if(event.keyCode == 81) {
+	 	setTimeout(function (){
+	 	$('#imgq').hide();
+		}, 5000);
+	 	}
+	 	if(event.keyCode == 82) {
+	 	setTimeout(function (){
+	 	$('#imgr').hide();
+		}, 4000);
+	 	}
+	 	if(event.keyCode == 83) {
+	 	setTimeout(function (){
+	 	$('#imgs').hide();
+		}, 4500);
+	 	}
+	 	if(event.keyCode == 84) {
+	 	setTimeout(function (){
+	 	$('#imgt').hide();
+		}, 4500);
+	 	}
+	 	if(event.keyCode == 85) {
+	 	setTimeout(function (){
+	 	$('#imgu').hide();
+		}, 7500);
+	 	}
+	 	if(event.keyCode == 86) {
+	 	setTimeout(function (){
+	 	$('#imgv').hide();
+		}, 3500);
+	 	}
+	 	if(event.keyCode == 87) {
+	 	setTimeout(function (){
+	 	$('#imgw').hide();
+		}, 5000);
+	 	}
+	 	if(event.keyCode == 88) {
+	 	setTimeout(function (){
+	 	$('#imgx').hide();
+		}, 4500);
+	 	}
+	 	if(event.keyCode == 89) {
+	 	setTimeout(function (){
+	 	$('#imgy').hide();
+		}, 4500);
+	 	}
+	 	if(event.keyCode == 90) {
+	 	setTimeout(function (){
+	 	$('#imgz').hide();
+		}, 9000);
+	 	}
+		});   
+	});
+
+
